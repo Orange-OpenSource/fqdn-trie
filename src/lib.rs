10696b6fe5204b16113ab6596125af9cbb1ca68a
Xophe <christophe.dousson@orange.com>
2025-01-23 23:10:08 +0100

    add '#' as special char

--- src/lib.rs
+++ src/lib.rs
@@ -21,17 +21,17 @@ trait HasFqdn {
 }
 
 // size of the alphabet
-pub(crate) const ALPHABET_SIZE: usize = 39; // we should also count the '_'
+pub(crate) const ALPHABET_SIZE: usize = 40; // we should also count the '_' and '#'
 
 // in order to decrease the necessary memory, this table reduces the search space only
-// to allowed chars in FQDN, i.e. a-z, A-Z, 0-9, - and _.
+// to allowed chars in FQDN, i.e. a-z, A-Z, 0-9, -, _ and #.
 // all the others are treated equally (i.e. as a dot)
 // this is case insensitive (lower and upper case give the same index)
 
 pub(crate) const ALPHABET: [u8;256] = [
     0, 0, 0, 0, 0, 0, 0, 0,      0, 0, 0, 0, 0, 0, 0, 0,   //  16
     0, 0, 0, 0, 0, 0, 0, 0,      0, 0, 0, 0, 0, 0, 0, 0,   //  32
-    0, 0, 0, 0, 0, 0, 0, 0,      0, 0, 0, 0, 0,37, 0, 0,   //  48 (-)
+    0, 0, 0,39, 0, 0, 0, 0,      0, 0, 0, 0, 0,37, 0, 0,   //  48 (# et -)
     27,28,29,30,31,32,33,34,    35,36, 0, 0, 0, 0, 0, 0,   //  64 (0-9)
     0, 1, 2, 3, 4, 5, 6, 7,      8, 9,10,11,12,13,14,15,   //  80 (A-O)
     16,17,18,19,20,21,22,23,    24,25,26, 0, 0, 0, 0,38,   //  96 (P-Z et _)
--- src/mapping.rs
+++ src/mapping.rs
@@ -14,9 +14,11 @@
 
 // value of the _ depends if we apply the rfc strictly or not
 #[cfg(feature="domain-name-without-special-chars")] const UNDER: (u8,u8) = (0,0);
+#[cfg(feature="domain-name-without-special-chars")] const HASH: (u8,u8) = (0,0);
 #[cfg(not(feature="domain-name-without-special-chars"))] const UNDER: (u8,u8) = (1,2);
+#[cfg(not(feature="domain-name-without-special-chars"))] const HASH: (u8,u8) = (1,3);
 
-// if more special chars are needed, the remaining available codes are: (1,3), (1,4), (1,5), (1,6)
+// if more special chars are needed, the remaining available codes are: (1,4), (1,5), (1,6)
 // (this should be have a negligible impact on the memory use and on performances)
 
 // if it is not enough, then the code (0,1) to (0,6) could be used
@@ -25,7 +27,7 @@
 const MAPPING: [(u8,u8);256] = [
     (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0),     (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0),
     (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0),     (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0),
-    (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0),     (0,0), (0,0), (0,0), (0,0), (0,0), (3,3), (0,0), (0,0), // '-' (dash)
+    (0,0), (0,0), (0,0),  HASH, (0,0), (0,0), (0,0), (0,0),     (0,0), (0,0), (0,0), (0,0), (0,0), (3,3), (0,0), (0,0), // '-' (dash)
     (3,5), (3,4), (3,6), (2,1), (2,3), (2,4), (2,5), (2,2),     (1,1), (2,6), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), // 0-9
     (0,0), (6,3), (5,4), (2,0), (6,5), (6,2), (4,1), (5,3),     (5,6), (6,1), (4,6), (4,0), (5,5), (5,0), (6,0), (1,0), // A-O
     (5,2), (3,1), (6,6), (5,1), (6,4), (3,0), (4,2), (4,4),     (4,5), (4,3), (3,2), (0,0), (0,0), (0,0), (0,0), UNDER, // P-Z and '_' (underscore)
